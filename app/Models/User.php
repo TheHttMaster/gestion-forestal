0f64714eed3f25f544ccf85814f9b09ea7986f7c
TheHttMaster <diperishilla2468@gmail.com>
2025-08-02 16:53:57 -0400

    Revierto a la versión del 2 Ago (menu espaciado textos)

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
-use Illuminate\Database\Eloquent\SoftDeletes; 
+
 
 class User extends Authenticatable{
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes, LogsActivity;
+    use HasFactory, Notifiable,  LogsActivity;
 
     /**
      * The attributes that are mass assignable.
