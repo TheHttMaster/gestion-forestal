d78ec1533ed4c6fa6aa70decf9323c8402538211
Geral Serrano <131737348+Andriu28@users.noreply.github.com>
2025-08-03 15:20:46 -0400

    se agrego boton de regresar se agrado este boton en la vista de crear usuruaio y de editar usuario

---
+++ resources/views/components/go-back-button.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <a href="javascript:history.back()" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+        Regresar
+    </a>
+</div>
