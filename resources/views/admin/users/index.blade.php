d36473c5affd46d7b47e7418259be6ed565c5e32
Geral Serrano <131737348+Andriu28@users.noreply.github.com>
2025-08-03 15:42:09 -0400

    CAMBIOS

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -56,7 +56,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                         <a href="{{ route('admin.users.edit', $user) }}" class="text-indigo-600 hover:text-indigo-900">Editar</a>
                                 
-                                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST" onsubmit="return confirm('¿Estás seguro de que quieres eliminar a este usuario?');">
+                                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST" onsubmit="return confirm('¿Estás seguro de que desea Deshabiliar a este usuario?');">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="text-red-600 hover:text-red-900">Deshabilitar</button>
