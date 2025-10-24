e6d915604f7f5517d1530f261f0f39769236494b
TheHttMaster <diperishilla2468@gmail.com>
2025-10-23 22:42:28 -0400

    cambios sobre la modal de añadir cordenadas manuales

--- resources/views/deforestation/create.blade.php
+++ resources/views/deforestation/create.blade.php
@@ -129,8 +129,8 @@
                     </div>
 
                 <!-- Modal moderno para coordenadas -->
-                    <div id="manual-polygon-modal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 hidden">
-                        <div class="bg-white dark:bg-custom-gray rounded-xl shadow-2xl w-full max-w-md mx-4">
+                    <div id="manual-polygon-modal" class=" hidden">
+                        <div class=" w-full max-w-md mx-4">
                             <!-- Header -->
                             <div class="flex justify-between items-center p-6 border-b border-gray-200 dark:border-gray-600">
                                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Ingresar Coordenadas</h3>
