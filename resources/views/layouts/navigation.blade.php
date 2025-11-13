745de5339591cfe2b73b608f42e053f745e65286
TheHttMaster <diperishilla2468@gmail.com>
2025-11-13 04:48:24 -0400

    cambios para quitar el modulo de area

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -66,11 +66,11 @@
                         </li>
                         <li>
                             <div class="transition-all duration-300 hover:scale-[1.03] hover:-translate-y-0.5 mb-2">
-                                <a href="{{ route('areas.index') }}" class="nav-item {{ request()->routeIs('areas.index') ? 'active' : '' }} transition-all duration-300 ease-in-out hover:font-bold hover:shadow-md group">
+                                <a href="#" class="nav-item {{ request()->routeIs('areas.index') ? 'active' : '' }} transition-all duration-300 ease-in-out hover:font-bold hover:shadow-md group">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sprout transition-all duration-500 group-hover:scale-[1.15] group-hover-ml-2">
                                         <path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0"/><circle cx="12" cy="8" r="2"/><path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712"/>
                                     </svg>
-                                    <span class="sidebar-text whitespace-nowrap">Areas de cultivo</span>
+                                    <span class="sidebar-text whitespace-nowrap">Areas de cultivo X</span>
                                 </a>
                             </div>
                         </li>
